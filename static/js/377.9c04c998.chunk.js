0bfcd07a2fb5f8b9bd57b2f550655525c402f954
Mamont777 <105058741+Mamont777@users.noreply.github.com>
2023-06-19 15:08:23 +0000

    Deploying to gh-pages from @ Mamont777/goit-react-hw-08-phonebook@7bb911279acedb580ea60759dcf06b56c907b73b 🚀

---
+++ static/js/377.9c04c998.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[377],{5377:function(e,r,t){t.r(r);var n=t(1087),o=t(3329);r.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h2",{children:"Sorry, something went wrong (:"}),(0,o.jsx)(n.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=377.9c04c998.chunk.js.map
