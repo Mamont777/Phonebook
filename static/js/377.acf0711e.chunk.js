0e4ba0342d5c81f1248a544460b21c6945331fa1
Mamont777 <105058741+Mamont777@users.noreply.github.com>
2023-06-27 10:36:35 +0000

    Deploying to gh-pages from @ Mamont777/Phonebook@cfaea047f33ed061a0f13c3f68133e957417b4bd 🚀

---
+++ static/js/377.acf0711e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[377],{5377:function(e,n,o){o.r(n);var r=o(1087),h=o(3329);n.default=function(){return(0,h.jsxs)(h.Fragment,{children:[(0,h.jsx)("h2",{children:"Sorry, something went wrong (:"}),(0,h.jsx)(r.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=377.acf0711e.chunk.js.map
